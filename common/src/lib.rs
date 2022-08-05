396d5caeed581e5d7d49e2a9d27f361592f5f0db
Denis <daletskidenis@gmail.com>
2022-08-06 01:11:48 +0200

    stream of characters task solved

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,2 +1,23 @@
 pub mod linked_list;
 pub mod trie;
+
+#[macro_export]
+macro_rules! assert_returns {
+    // This macro takes an expression of type `expr` and prints
+    // it as a string along with its result.
+    // The `expr` designator is used for expressions.
+    ($ret_value:expr, $func:expr, $($args:expr),*) => {
+        // `stringify!` will convert the expression *as it is* into a string.
+        let mut error_msg = format!(
+            "expected result: {:?},\nfunction: {:?},\nargs:",
+            $ret_value,
+            stringify!($func),
+            );
+        $(
+            error_msg += format!("\n  {:?}", $args).as_str();
+        )*
+            error_msg += "\n";
+
+        assert_eq!($func($($args),*), $ret_value, "\n{:}", error_msg);
+    };
+}
--- common/src/trie.rs
+++ common/src/trie.rs
@@ -145,28 +145,8 @@ mod tests {
         prelude::Distribution,
     };
     use rstest::{fixture, rstest};
-
     use super::Trie;
-
-    macro_rules! assert_returns {
-        // This macro takes an expression of type `expr` and prints
-        // it as a string along with its result.
-        // The `expr` designator is used for expressions.
-        ($ret_value:expr, $func:expr, $($args:expr),*) => {
-            // `stringify!` will convert the expression *as it is* into a string.
-            let mut error_msg = format!(
-                "expected result: {:?},\nfunction: {:?},\nargs:",
-                $ret_value,
-                stringify!($func),
-                );
-            $(
-                error_msg += format!("\n  {:?}", $args).as_str();
-            )*
-                error_msg += "\n";
-
-            assert_eq!($func($($args),*), $ret_value, "\n{:}", error_msg);
-        };
-    }
+    use crate::assert_returns;
 
     #[fixture]
     fn empty_trie() -> Trie {
