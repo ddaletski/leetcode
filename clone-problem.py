9f99b52dbbd654c6ed674c9867b1f04c17956077
Denis Daletski <daletskidenis@gmail.com>
2022-10-24 12:33:55 +0200

    problem clonning script

---
+++ clone-problem.py
@@ -0,0 +1,37 @@
+import click
+import sys
+from pathlib import Path
+from urllib.parse import urlparse
+import subprocess
+
+
+@click.command()
+@click.argument("url", type=str)
+def clone_problem(url: str):
+    script_dir = Path(sys.argv[0]).parent
+    problems_dir = script_dir / "problems"
+
+    problem_name = urlparse(url).path.strip("/").split("/")[-1]
+    print(f"clonning {problem_name}")
+
+    initialize_problem_dir(problems_dir / problem_name)
+
+
+def initialize_problem_dir(problem_dir: Path):
+    try:
+        problem_dir.mkdir()
+    except FileExistsError:
+        print("problem is already clonned")
+        return
+
+    def run_command(cmd: str):
+        subprocess.run(cmd, shell=True,
+                       check=True, cwd=problem_dir.absolute())
+
+    run_command("cargo init --lib")
+    run_command("cargo add --dev rstest")
+    run_command("cargo add --dev --path=../../common")
+
+
+if __name__ == "__main__":
+    clone_problem()
