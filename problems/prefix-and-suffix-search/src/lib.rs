06e38de5ddaef1e397c2351e1e6c2695088a9f61
Denis Daletski <daletskidenis@gmail.com>
2023-04-08 21:51:07 +0200

    faster prefix-and-suffix-search

--- problems/prefix-and-suffix-search/src/lib.rs
+++ problems/prefix-and-suffix-search/src/lib.rs
@@ -2,13 +2,13 @@ use std::collections::{HashMap, HashSet};
 
 use common::trie::Trie;
 
-struct WordFilter {
+struct WordFilter1 {
     forward_trie: Trie,
     backward_trie: Trie,
     index_mapping: HashMap<String, i32>,
 }
 
-impl WordFilter {
+impl WordFilter1 {
     fn new(words: Vec<String>) -> Self {
         let mut fw = Trie::new();
         let mut bw = Trie::new();
@@ -53,6 +53,66 @@ impl WordFilter {
         max_index
     }
 }
+
+struct WordFilter2 {
+    prefixes: HashMap<String, HashSet<u16>>,
+    suffixes: HashMap<String, HashSet<u16>>,
+}
+
+impl WordFilter2 {
+    fn new(words: Vec<String>) -> Self {
+        let words: HashMap<String, i32> = words
+            .into_iter()
+            .enumerate()
+            .map(|(idx, word)| (word, idx as i32))
+            .collect();
+
+        let mut prefixes: HashMap<String, HashSet<u16>> = HashMap::new();
+
+        for (word, &word_idx) in words.iter() {
+            for pref_len in 1..=7 {
+                if word.len() < pref_len {
+                    continue;
+                }
+
+                let prefix: String = word.chars().take(pref_len).collect();
+
+                prefixes.entry(prefix).or_default().insert(word_idx as u16);
+            }
+        }
+
+        let mut suffixes: HashMap<String, HashSet<u16>> = HashMap::new();
+
+        for (word, &word_idx) in words.iter() {
+            for suff_len in 1..=7 {
+                if word.len() < suff_len {
+                    continue;
+                }
+
+                let suffix: String = word
+                    .bytes()
+                    .skip(word.len() - suff_len)
+                    .map(|byte| byte as char)
+                    .collect();
+
+                suffixes.entry(suffix).or_default().insert(word_idx as u16);
+            }
+        }
+
+        Self { prefixes, suffixes }
+    }
+
+    fn f(&self, pref: String, suff: String) -> i32 {
+        let prefix_set = self.prefixes.get(&pref).cloned().unwrap_or_default();
+        let suffix_set = self.suffixes.get(&suff).cloned().unwrap_or_default();
+
+        let common_set = prefix_set.intersection(&suffix_set);
+
+        common_set.max().cloned().map(i32::from).unwrap_or(-1)
+    }
+}
+
+type WordFilter = WordFilter2;
 
 #[cfg(test)]
 mod tests {
